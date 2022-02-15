0ca84a7e0bceb5ada029163ae16c325f97cfc4cd
pranay-j <57652119+pranay-j@users.noreply.github.com>
2022-02-15 17:06:06 +0530

    testcade 1 for displaying product name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe("shopping cart stories",()=>{
+it('renders learn react link', () => {
   const inputAppName="Manyvar Shop";
   render(<App appName={inputAppName}/>);
 
   const shoppingCartHeaderElement = screen.getByRole("heading",{name:inputAppName})
 
   expect(shoppingCartHeaderElement).toBeInTheDocument();
+});
+
+it.only('should display product name', () => {
+  render(<App appName={'inputAppName'} />);
+  const nameElement = screen.getByText(/Laptop Bagpack/);
+  expect(nameElement).toBeInTheDocument();
+});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const  App:React.FC<AppProps> = ({appName})=> {
     <div>
      <h1>{appName}</h1>
     </div>
+    
   );
 }
 
