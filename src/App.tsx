42a4d316e123511bb1fa55243a9a42317336d7a3
pranay-j <57652119+pranay-j@users.noreply.github.com>
2022-02-15 17:18:20 +0530

    implemented code for displaying product name

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 interface AppProps{
-  appName:string
+  appName:string,
 }
 
 const  App:React.FC<AppProps> = ({appName})=> {
   return (
     <div>
      <h1>{appName}</h1>
+     <h5>Laptop Bagpack</h5>
     </div>
-    
+   
   );
 }
 
