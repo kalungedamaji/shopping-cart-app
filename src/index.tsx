91a3100ce608d8c6b657a4ba897cdef5bab1524e
Abhijit Hycinth <abhijeethycinth@gmail.com>
2022-02-18 12:24:05 +0530

    updated app name

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App appName="Manyavar Shop"/>,
+ReactDOM.render(<App appName="Amazon for Technogise"/>,
    document.getElementById("root"));
