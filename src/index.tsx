e8b18ceff01f994d453d561f6530f2a148b1c848
shrishti-27 <shrishtijain1345@gmail.com>
2022-02-18 12:31:12 +0530

    Updated appName

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App appName="Amazon for Technogise"/>,
+ReactDOM.render(<App appName="Flipcart for Technogise"/>,
    document.getElementById("root"));
