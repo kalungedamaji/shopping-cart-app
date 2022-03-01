9f2bbb22c8d503973d5bfacf443dd5e9f06e0354
VISHAL RAJ <vishalrajput97084@gmail.com>
2022-03-01 15:34:54 +0530

    Implemented functionality for increment button

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductDetail } from "../product-store/Product";
 
 export interface CartProps {
   productDetail: ProductDetail;
 }
 
+
 const Cart: React.FC<CartProps> = ({ productDetail }) => {
 
-  let quantity = 1;
+  const [quantity, setQuantity] = useState(1);
 
-
+  function incrementHandler() {
+    setQuantity(quantity+1);
+    }
 
   return (
       <div>
@@ -18,8 +21,8 @@ const Cart: React.FC<CartProps> = ({ productDetail }) => {
         <p>Price : {productDetail.price}</p>
         <button>-</button>
         <p>Quantity : {quantity}</p>
-        <button>+</button>
-        <p>Total : {+productDetail.price * quantity}</p>
+        <button onClick={incrementHandler}>+</button>
+        <p>Total : {productDetail.price * quantity}</p>
       </div>
   );
 };
